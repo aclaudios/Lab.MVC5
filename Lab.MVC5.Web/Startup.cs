cf071a7878205f9f0d0d56b3112763986e14ef73
Alessandro Claudio da Silva <aclaudios@hotmail.com>
2016-03-17 17:18:54 -0300

    Initial

---
+++ Lab.MVC5.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab.MVC5.Web.Startup))]
+namespace Lab.MVC5.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
